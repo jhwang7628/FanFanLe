8f38b6d5db501d3bc0ce6e39e264b45feff44432
Jui-Hsien Wang <jhwang.cornell@gmail.com>
2016-01-06 23:17:30 -0800

    project starts\!

---
+++ src/graphics.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python 
+
+###################################
+##  This code is part of FanFanLe
+##      Jui-Hsien Wang
+##      Jan 2016
+###################################
+
+# graphics support 
---
+++ src/main.py
@@ -0,0 +1,9 @@
+#!/usr/bin/eny python 
+
+###################################
+##  This code is part of FanFanLe
+##      Emma Wang
+##      Jan 2016
+###################################
+
+# main implementation of FanFanLe
